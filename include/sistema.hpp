ce2afb291fb10291f7615b1b2fe36fc1eef006bb
CalebeAF02 <calebe.2324@gmail.com>
2025-12-07 16:56:44 -0300

    Criando Projeto

---
+++ include/sistema.hpp
@@ -0,0 +1,14 @@
+//
+// Created by caleb on 07/12/2025.
+//
+
+#ifndef UNTITLED_SISTEMA_HPP
+#define UNTITLED_SISTEMA_HPP
+
+
+class sistema
+{
+};
+
+
+#endif //UNTITLED_SISTEMA_HPP
---
+++ src/sistema.cpp
@@ -0,0 +1,5 @@
+//
+// Created by caleb on 07/12/2025.
+//
+
+#include "../include/sistema.hpp"
